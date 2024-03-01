f05dc6b8dbd2a87f49b57dfadd7e243e420a725b
Gabriel Egli <gabriel.j.egli+git@gmail.com>
2024-03-01 17:51:21 +0100

    fixed tailwindcss

--- apps/app/tailwind.config.ts
+++ apps/app/tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 import sharedConfig from "@repo/tailwind-config";
 
 const config: Pick<Config, "content" | "presets"> = {
-	content: ["./src/app/**/*.tsx"],
+	content: ["./app/**/*.tsx"],
 	presets: [sharedConfig],
 };
 
