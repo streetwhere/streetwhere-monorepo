35d8e0b7d80f7af736ca90a0a591b0636d3a05e2
Turbobot <turbobot@vercel.com>
2024-03-03 00:52:05 +0100

    feat(create-turbo): apply official-starter transform

--- apps/app/tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "tailwindcss";
-import sharedConfig from "@repo/tailwind-config";
-
-const config: Pick<Config, "content" | "presets"> = {
-	content: ["./app/**/*.tsx"],
-	presets: [sharedConfig],
-};
-
-export default config;
